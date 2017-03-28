fd0118b6ec062dd22124d439be8b6e4dd946252b
Andrey Kozhurin <nice@mac-mini-andrey.local>
2017-03-28 20:40:29 +0300

    git init

---
+++ Framework_Test/Framework_Test.h
@@ -0,0 +1,19 @@
+//
+//  Framework_Test.h
+//  Framework_Test
+//
+//  Created by Andrey Kozhurin on 28/03/2017.
+//  Copyright © 2017 Andrey Kozhurin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Framework_Test.
+FOUNDATION_EXPORT double Framework_TestVersionNumber;
+
+//! Project version string for Framework_Test.
+FOUNDATION_EXPORT const unsigned char Framework_TestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Framework_Test/PublicHeader.h>
+
+

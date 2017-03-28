e7103994db6e14ffcebe7abbb0efec1b69b0ef55
Andrey Kozhurin <nice@mac-mini-andrey.local>
2017-03-28 20:49:02 +0300

    MyClass class

---
+++ Framework_Test/Model/MyClass.swift
@@ -0,0 +1,20 @@
+//
+//  MyClass.swift
+//  Framework_Test
+//
+//  Created by Andrey Kozhurin on 28/03/2017.
+//  Copyright © 2017 Andrey Kozhurin. All rights reserved.
+//
+
+import Foundation
+
+class MyClass
+{
+    var var1: Int = 1
+    var var2: String = "2"
+    
+    func show()
+    {
+        print(var1, var2)
+    }
+}
